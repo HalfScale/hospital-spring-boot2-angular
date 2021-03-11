7348d0ea96b2eb19f52e642601d90f80a9607d36
MacMuffin <halfscale3@gmail.com>
2021-03-11 08:33:52 +0800

    Enhancment of UI

--- client/src/app/interceptors/token-interceptor.ts
+++ client/src/app/interceptors/token-interceptor.ts
@@ -1,57 +1,65 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable, throwError } from "rxjs";
-import { catchError, map, switchMap } from "rxjs/operators";
+import { catchError, map, switchMap, filter, take } from "rxjs/operators";
 import { JwtAuthResponse } from "../entities/jwt-auth-response";
 import { AuthService } from "../services/auth.service";
 
 @Injectable({
     providedIn: 'root'
 })
-export class TokenInterceptor implements HttpInterceptor{
+export class TokenInterceptor implements HttpInterceptor {
 
     isTokenRefreshing = false;
     refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject(null);
 
     constructor(public authService: AuthService) {
-        
+
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-        const jwtToken = this.authService.getJwtToken();
-        if(jwtToken) {
-            this.addToken(req, jwtToken);
+        if (req.url.indexOf('refresh') !== -1 || req.url.indexOf('login') !== -1) {
+            return next.handle(req);
         }
 
-        return next.handle(req).pipe(catchError(error => {
-            if(error instanceof HttpErrorResponse && error.status == 403) {
-                return this.handleAuthErrors(req, next); // Will perform another call for refreshing tokens
-            }else {
+        const jwtToken = this.authService.getJwtToken();
+
+        return next.handle(this.addToken(req, jwtToken)).pipe(catchError(error => {
+            if (error instanceof HttpErrorResponse && error.status === 403) {
+                return this.handleAuthErrors(req, next);
+            } else {
                 return throwError(error);
             }
         }))
     }
 
     // error handling to request a call again in the back end
-    private handleAuthErrors(req: HttpRequest<any>, next: HttpHandler)
-        : Observable<HttpEvent<any>> {
+    private handleAuthErrors(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-            if(!this.isTokenRefreshing) {
-                this.isTokenRefreshing = true;
-                this.refreshTokenSubject.next(null);
+        if (!this.isTokenRefreshing) {
+            this.isTokenRefreshing = true;
+            this.refreshTokenSubject.next(null);
 
-                return this.authService.refreshToken()
-                    .pipe(map(response => response),
+            return this.authService.refreshToken()
+                .pipe(map(response => response),
                     switchMap((response) => {
                         this.isTokenRefreshing = false;
                         this.refreshTokenSubject.next(response.authToken);
 
                         return next.handle(this.addToken(req, response.authToken));
                     }));
-            }
-            
-        return new Observable();
+        }else {
+            return this.refreshTokenSubject.pipe(
+                filter(result => result !== null),
+                take(1),
+                switchMap((res) => {
+                    return next.handle(this.addToken(req, this.authService.getJwtToken()))
+                })
+            );
+        }
+
+
     }
 
     addToken(req: HttpRequest<any>, jwtToken: any) {
