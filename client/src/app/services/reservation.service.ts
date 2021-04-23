9996beb9f021782475d52bf05037c310226db5aa
MacMuffin <halfscale3@gmail.com>
2021-04-23 23:49:17 +0800

    Room reservation update

--- client/src/app/services/reservation.service.ts
+++ client/src/app/services/reservation.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GlobalVariable } from '../globals';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  private apiUrl = GlobalVariable.BASE_API_URL;
+
+  constructor(private http: HttpClient) { }
+
+  public getAvailableReservationDate(): Observable<any> {
+    return this.http.get<any>(this.apiUrl + "/api/reservations/available_date");
+  }
 }
